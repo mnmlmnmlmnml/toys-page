2ef70427326e4ee9d796ecf3291d82865d0ee311
DARYA <darya.aleks.2014@mail.ru>
2021-09-14 12:57:47 +0300

    ci: add gh-pages, refs #10

--- src/App.js
+++ src/App.js
@@ -1,19 +1,13 @@
 import React from 'react'
-import { BrowserRouter, Route, Switch } from 'react-router-dom'
-import styles from './App.module.scss'
 import { Main } from './pages/Main'
-
 import { Header } from './features/Header'
+import styles from './App.module.scss'
 
 function App() {
     return (
         <div className={styles.App}>
-            <BrowserRouter>
-                <Header />
-                <Switch>
-                    <Route path='/' exact component={Main} />
-                </Switch>
-            </BrowserRouter>
+            <Header />
+            <Main />
         </div>
     )
 }
